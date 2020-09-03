5d1ba5be36c1dd990c64c9fd1ff572c8282e47c5
BrandonEJenkins <brandonejenkins@gmail.com>
2020-09-02 21:00:17 -0400

    Updates

---
+++ precache-manifest.1cdb3e26218c7614967a2eeb8616196a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3502361cd68a41e6436eafd0166ba31",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "1096dc5ba3ad5efde5f3",
+    "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "65a1c7a9a1d7d8d9f2d7",
+    "url": "/react-portfolio/static/css/main.b4ded466.chunk.css"
+  },
+  {
+    "revision": "1096dc5ba3ad5efde5f3",
+    "url": "/react-portfolio/static/js/2.5e2515c3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.5e2515c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65a1c7a9a1d7d8d9f2d7",
+    "url": "/react-portfolio/static/js/main.4a66d12f.chunk.js"
+  },
+  {
+    "revision": "c0d330ec59ab0a555019",
+    "url": "/react-portfolio/static/js/runtime-main.91324e1c.js"
+  },
+  {
+    "revision": "b4dd1ae0ebcf90da492bb873e50f6f7b",
+    "url": "/react-portfolio/static/media/BeerFinder.b4dd1ae0.png"
+  },
+  {
+    "revision": "c16dc673a549c1fc84812df4b87bc443",
+    "url": "/react-portfolio/static/media/Good-ReadMe-Generator-App.c16dc673.png"
+  },
+  {
+    "revision": "1018ff0606e8df74ab6a0174636d1852",
+    "url": "/react-portfolio/static/media/MySQL_Emp_Mngr_App.1018ff06.png"
+  },
+  {
+    "revision": "fe6721d38f9184610983e0da75bc09c5",
+    "url": "/react-portfolio/static/media/Note Taker App.fe6721d3.png"
+  },
+  {
+    "revision": "e84e1e12d24469aec26b6b64a74a6ad6",
+    "url": "/react-portfolio/static/media/WeatherDashboard.e84e1e12.png"
+  },
+  {
+    "revision": "92af9b3abcd09e8387d87b570c6cca41",
+    "url": "/react-portfolio/static/media/passwordGen.92af9b3a.png"
+  }
+]);
